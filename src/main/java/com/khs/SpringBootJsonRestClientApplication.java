082d572e3ef5fddd5fd2495f05c6c7fefd3b23f7
Joe <jverderber@keyholesoftware.com>
2018-01-29 03:53:32 -0600

    .

---
+++ src/main/java/com/khs/SpringBootJsonRestClientApplication.java
@@ -0,0 +1,12 @@
+package com.khs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJsonRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJsonRestClientApplication.class, args);
+	}
+}
